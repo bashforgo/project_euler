087a046a539e38f4a58bade7b8963b7d1572c2f5
Barnabas Forgo <speedo@devo.hu>
2019-07-21 22:18:54 +0100

    problem 1

--- src/problems/p001.rs
+++ src/problems/p001.rs
@@ -1,3 +1,7 @@
 pub fn solve() -> String {
-    String::from("123")
+    (0..1000)
+        .into_iter()
+        .filter(|x| x % 3 == 0 || x % 5 == 0)
+        .sum::<i32>()
+        .to_string()
 }
