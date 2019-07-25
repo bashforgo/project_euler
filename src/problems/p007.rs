00ceef44599b2f29d11a2759bd37a6a37ff85310
Barnabas Forgo <speedo@devo.hu>
2019-07-25 15:43:24 +0100

    problem 7

---
+++ src/problems/p007.rs
@@ -0,0 +1,51 @@
+#[inline(always)]
+fn divs(n: i32, m: i32) -> bool {
+    n % m == 0
+}
+
+fn is_prime(n: i32) -> bool {
+    if n <= 3 {
+        return n > 1;
+    } else if divs(n, 2) || divs(n, 3) {
+        return false;
+    }
+
+    let mut i: i32 = 5;
+    while i.pow(2) <= n {
+        if divs(n, i) || divs(n, i + 2) {
+            return false;
+        }
+
+        i += 6;
+    }
+
+    return true;
+}
+
+pub fn solve() -> String {
+    (2..)
+        .into_iter()
+        .filter(|n| is_prime(*n))
+        .skip(10000)
+        .take(1)
+        .sum::<i32>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_few_primes() {
+        let primes: Vec<_> = (2..)
+            .into_iter()
+            .filter(|n| is_prime(*n))
+            .take(20)
+            .collect();
+        assert_eq!(
+            primes,
+            vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+        );
+    }
+}
