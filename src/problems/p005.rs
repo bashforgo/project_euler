ae0988d118fb7955b78b108200d7fbcd7ef75771
Barnabas Forgo <speedo@devo.hu>
2019-07-25 20:42:57 +0100

    non-brute-force problem 5

--- src/problems/p005.rs
+++ src/problems/p005.rs
@@ -1,14 +1,38 @@
-const PRIMES: [i32; 8] = [2, 3, 5, 7, 11, 13, 17, 19];
+fn gcd(a: i32, b: i32) -> i32 {
+    let mut a = a;
+    let mut b = b;
+
+    while b != 0 {
+        let tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+
+    a
+}
+
+fn lcm(a: i32, b: i32) -> i32 {
+    (a * b) / gcd(a, b)
+}
 
 pub fn solve() -> String {
-    (1..)
+    (2..20)
         .into_iter()
-        // .filter(|n| (2..20).into_iter().all(|m| n % m == 0))
-        .filter(|n| n % 20 == 0)
-        .filter(|n| n % 18 == 0)
-        .filter(|n| n % 16 == 0)
-        .filter(|n| PRIMES.iter().all(|p| n % p == 0))
-        .take(1)
-        .sum::<i32>()
+        .fold(1, |acc, cur| lcm(acc, cur))
         .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _gcd() {
+        assert_eq!(gcd(1071, 462), 21);
+    }
+
+    #[test]
+    fn _lcm() {
+        assert_eq!(lcm(21, 6), 42);
+    }
 }
