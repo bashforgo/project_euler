0156269d06b4e23e754a8e05e13504dfbed56a4f
Barnabas Forgo <speedo@devo.hu>
2019-09-24 22:00:15 +0100

    generate permutations in order

--- src/problems/p024.rs
+++ src/problems/p024.rs
@@ -1,56 +1,41 @@
-struct Permutation {
-    v: Vec<char>,
-    c: Vec<usize>,
-    i: usize,
-    first: bool,
-}
-
-impl Permutation {
-    fn new(v: Vec<char>) -> Permutation {
-        let c = v.iter().map(|_| 0).collect::<Vec<_>>();
-        let i = 0;
-        let first = true;
-
-        Permutation { v, c, i, first }
-    }
-}
-
-impl Iterator for Permutation {
-    type Item = Vec<char>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.first {
-            self.first = false;
-            Some(self.v.clone())
-        } else if self.i < self.v.len() {
-            if self.c[self.i] < self.i {
-                if self.i % 2 == 0 {
-                    self.v.swap(0, self.i);
-                } else {
-                    self.v.swap(self.c[self.i], self.i);
-                }
-
-                self.c[self.i] += 1;
-                self.i = 0;
-
-                Some(self.v.clone())
-            } else {
-                self.c[self.i] = 0;
-                self.i += 1;
-                self.next()
-            }
-        } else {
-            None
-        }
-    }
-}
-
 pub fn solve() -> String {
-    let digits = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let mut digits = *b"0123456789";
 
-    let p = Permutation::new(digits);
-    let mut p = p.map(|p| p.iter().collect::<String>()).collect::<Vec<_>>();
-    p.sort();
+    let mut n = 0;
+    let mut res = None;
+    loop {
+        n += 1;
+        if n == 1_000_000 {
+            res = Some(String::from_utf8_lossy(&digits).to_string());
+            break;
+        }
 
-    p[1_000_000 - 1].to_string()
+        let mut first_char = None;
+        for i in (1..digits.len()).rev() {
+            if digits[i - 1] < digits[i] {
+                first_char = Some(i - 1);
+                break;
+            }
+        }
+
+        if first_char.is_none() {
+            break;
+        }
+
+        let first_char = first_char.unwrap();
+
+        let mut ceil = first_char + 1;
+        for i in first_char + 1..digits.len() {
+            if digits[i] > digits[first_char] && digits[i] < digits[ceil] {
+                ceil = i;
+            }
+        }
+
+        digits.swap(first_char, ceil);
+
+        let right_of_first = &mut digits[first_char + 1..];
+        right_of_first.reverse();
+    }
+
+    res.unwrap()
 }
